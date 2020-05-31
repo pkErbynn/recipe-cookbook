bc80e23f4658abc19d174a69373ea7989bbd26ba
pkerbynn <john.erbynn@gmail.com>
2020-05-31 04:54:16 +0000

    inject data storage

--- src/app/recipes/recipes-resolver.server.ts
+++ src/app/recipes/recipes-resolver.server.ts
@@ -2,8 +2,11 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Resolve } from "@angular/router";
 
+import { DataStorageService } from "../shared/data-storage.service";
+
 @Injectable({ providedIn: "root" })
 export class RecipeResolverService implements Resolve<Recipe[]> {
+  constructor(dataStorageService: DataStorageService) {}
   resolve(
     route: import("@angular/router").ActivatedRouteSnapshot,
     state: import("@angular/router").RouterStateSnapshot
