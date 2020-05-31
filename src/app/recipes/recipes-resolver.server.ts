9662fa7d6afe1cbf76960e58096f8e3d20812f3e
pkerbynn <john.erbynn@gmail.com>
2020-05-31 04:55:42 +0000

    remove imports

--- src/app/recipes/recipes-resolver.server.ts
+++ src/app/recipes/recipes-resolver.server.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
-import { Resolve } from "@angular/router";
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
 
 import { DataStorageService } from "../shared/data-storage.service";
 
@@ -8,8 +12,8 @@ import { DataStorageService } from "../shared/data-storage.service";
 export class RecipeResolverService implements Resolve<Recipe[]> {
   constructor(dataStorageService: DataStorageService) {}
   resolve(
-    route: import("@angular/router").ActivatedRouteSnapshot,
-    state: import("@angular/router").RouterStateSnapshot
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
   ): Recipe[] | import("rxjs").Observable<Recipe[]> | Promise<Recipe[]> {
     throw new Error("Method not implemented.");
   }
