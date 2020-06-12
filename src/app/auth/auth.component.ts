e29bdc227c63804a34e1c90bd9201b03f3a2c90d
pkerbynn <john.erbynn@gmail.com>
2020-06-12 03:14:18 +0000

    add switchiin' btn auth mode

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,4 +4,9 @@ import { Component } from "@angular/core";
   selector: "app-auth",
   templateUrl: "./auth.component.html",
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
