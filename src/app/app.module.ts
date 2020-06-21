7864330ce72335ac77cf05b024d79dcc8432a6c8
pkerbynn <john.erbynn@gmail.com>
2020-06-21 10:13:45 +0000

    add spinner and error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeService } from "./recipes/recipe.service";
 import { AuthComponent } from "./auth/auth.component";
+import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { AuthComponent } from "./auth/auth.component";
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -24,13 +26,17 @@ export class AuthComponent {
     const email = authForm.value.email;
     const password = authForm.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
     } else {
       this.authService.signUp(email, password).subscribe(
         (res) => {
+          this.isLoading = false;
           console.log(res);
         },
         (error) => {
+          this.error = "An error has occurred!";
+          this.isLoading = false;
           console.log(error);
         }
       );
