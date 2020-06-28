d648d721bea844d3152481ff5d6b92804630ec5d
pkerbynn <john.erbynn@gmail.com>
2020-06-28 07:13:29 +0000

    store user token in localstorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -86,6 +86,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 
   private handlingError(errorRes: HttpErrorResponse) {
