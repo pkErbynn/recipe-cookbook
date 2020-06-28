99b77280885e7301af5ec4ef22ba54a62f73ca44
pkerbynn <john.erbynn@gmail.com>
2020-06-28 05:30:34 +0000

    add logout on header menus

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -70,6 +70,10 @@ export class AuthService {
       );
   }
 
+  logout() {
+    this.user.next(null);
+  }
+
   private handleAuthentication(
     email: string,
     userId: string,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -41,6 +41,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.dataStorageService.fetchRecipes().subscribe();
   }
 
+  onLogout(){
+    this.authService.logout() 
+  }
+
   ngOnDestroy() {
     this.userSub.unsubscribe();
   }
