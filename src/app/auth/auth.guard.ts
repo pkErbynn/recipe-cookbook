a3c235b54808eac34a4e62961e206ce6de6b93a9
pkerbynn <john.erbynn@gmail.com>
2020-07-02 06:59:33 +0000

    + route guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -6,9 +6,13 @@ import {
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { map } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -17,6 +21,12 @@ export class AuthGuard implements CanActivate {
     | UrlTree
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> {
-    throw new Error("Method not implemented.");
+    // no subscription here...just transformation of observable result
+    return this.authService.user.pipe(
+      map((user) => {
+        // convert valid user to true boolean, else false
+        return !!user;
+      })
+    );
   }
 }
