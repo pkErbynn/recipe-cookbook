b2cea3a3f93b6c59b32afcd86295db5fc5490915
pkerbynn <john.erbynn@gmail.com>
2020-07-02 06:39:14 +0000

    + route guard init

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    throw new Error("Method not implemented.");
+  }
+}
