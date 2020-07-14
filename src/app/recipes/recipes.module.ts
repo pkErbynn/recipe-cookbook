062bebfae75b81b3d04367da90f5acc27cfba193
pkerbynn <john.erbynn@gmail.com>
2020-07-14 07:08:04 +0000

    fix: + recipes module correctly

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "../app-routing.module";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports: [AppRoutingModule, ReactiveFormsModule, CommonModule],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+})
+export class RecipeModule {}
