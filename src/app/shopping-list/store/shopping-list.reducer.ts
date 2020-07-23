d8f486cb1a2e00c56bd8d8e582679e473dc3d96d
pkerbynn <john.erbynn@gmail.com>
2020-07-23 07:09:09 +0000

    fix: namespace and outsource action

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,12 +1,15 @@
-import { Ingredient } from "../shared/ingredient.model";
+import { Action } from "@ngrx/store";
+
+import { Ingredient } from "../../shared/ingredient.model";
+import { ADD_INGREDIENT } from "./shopping-list.actions";
 
 const initialState = {
   ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoe", 32)],
 };
 
-export function shoppingListReducer(state = initialState, action) {
+export function shoppingListReducer(state = initialState, action: Action) {
   switch (action.type) {
-    case "ADD_INGREDIENT":
+    case ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action],
