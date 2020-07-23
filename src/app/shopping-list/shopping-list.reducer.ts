89a0d09fe8df28e703a44cc63a0a3c599902d11a
pkerbynn <john.erbynn@gmail.com>
2020-07-23 06:56:25 +0000

    + logic to reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -3,4 +3,13 @@ import { Ingredient } from "../shared/ingredient.model";
 const initialState = {
   ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoe", 32)],
 };
-export function shoppingListReducer(state = initialState, action) {}
+
+export function shoppingListReducer(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_INGREDIENT":
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action],
+      };
+  }
+}
