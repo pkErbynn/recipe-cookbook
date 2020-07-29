c538799c03bd91c3ada5dd9280982fc4dc8f2f16
pkerbynn <john.erbynn@gmail.com>
2020-07-29 20:42:03 +0000

    + multi actions init

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,9 +2,17 @@ import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = "ADD_INGREDIENT"; // to avoid typo, rather import
+export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
 
 // extend Action with payload
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
+
   constructor(public payload: Ingredient) {}
+}
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+
+  constructor(public payload: Ingredient[]) {}
 }
